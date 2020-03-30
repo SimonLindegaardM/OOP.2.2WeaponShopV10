8b2d7ff4d575c2abe3739c8799a13af8a4f4cda8
SimonLindegaardM <62882405+SimonLindegaardM@users.noreply.github.com>
2020-03-30 13:38:28 +0200

    Initial commit

---
+++ Axe.cs
@@ -0,0 +1,40 @@
+﻿namespace WeaponShopV10
+{
+    /// <summary>
+    /// This class represents a Axe. An Axe is 
+    /// considered to be a weapon.
+    /// </summary>
+    public class Axe : Weapon
+    {
+        public const int InitialAxeMinDamage = 20;
+        public const int InitialAxeMaxDamage = 50;
+
+        #region Constructor
+        public Axe(string description) 
+            : base(description, InitialAxeMinDamage, InitialAxeMaxDamage)
+        {
+        } 
+        #endregion
+        public int DamageFromAxe()
+        {
+            int DealDamage =base.CalculateDamage();
+            base.MinDamage -= 3;
+            base.MaxDamage -= 3;
+            if(base.MinDamage <0)
+            {
+                base.MinDamage = 0;
+            }
+            if(base.MaxDamage <1)
+            {
+                base.MaxDamage = 1;
+            }
+            
+            return DealDamage;
+        }
+        public void Sharpen()
+        {
+            base.MinDamage=InitialAxeMinDamage;
+            base.MaxDamage=InitialAxeMaxDamage;
+        }
+    }
+}
---
+++ Wand.cs
@@ -0,0 +1,34 @@
+﻿namespace WeaponShopV10
+{
+    /// <summary>
+    /// This class represents a Wand. A Wand is 
+    /// considered to be a weapon.
+    /// </summary>
+    public class Wand : Weapon
+    {
+        public const int InitialWandMinDamage = 10;
+        public const int InitialWandMaxDamage = 30;
+        private bool _IsEnchanted;
+
+        #region Constructor
+        public Wand(string description) 
+            : base(description, InitialWandMinDamage, InitialWandMaxDamage)
+        {
+            _IsEnchanted = false;
+        } 
+        #endregion
+        public bool IsEnchanted
+        {
+            get { return _IsEnchanted;}
+            set { _IsEnchanted = value;}   
+        }
+        public int DamageFromWand()
+        {
+            if(_IsEnchanted)
+            {
+                return base.CalculateDamage() * 2;
+            }
+            return base.CalculateDamage();
+        }
+    }
+}
